b222dbed6b7135671b586389bc9c92cad1435623
tran trung hieu <kazypos@gmail.com>
2016-08-05 23:26:35 +0700

    Hieu-Fix- print summary for admin
    - clear bảng mỗi khi user thay đổi giá trị lọc
    - fix câu lệnh lọc data trong model

--- Solution/HotelReservationSystem/Administration/View/PrintSummaryAdminView.cs
+++ Solution/HotelReservationSystem/Administration/View/PrintSummaryAdminView.cs
@@ -8,13 +8,13 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using HotelReservationSystem.Administration.Control;
-using System.Data;
 
 namespace HotelReservationSystem.Administration.View
 {
     public partial class PrintSummaryAdminView : Form
     {
         PrintSummaryAdminController control;
+        DataTable dataTable;
 
         private string hotelCode;
         private string roomType;
@@ -39,44 +39,65 @@ namespace HotelReservationSystem.Administration.View
             comboRoomType.ValueMember = "TypeCode";
             comboRoomType.DataSource = control.RoomTypes;
 
+            // clear hour, minute, second and just use to day, month and year
             String tmp = DateTime.Now.ToShortDateString();
             datePickerOut.Value = Convert.ToDateTime(tmp) + new TimeSpan(1, 0, 0, 0, 0);
             datePickerIn.Value = Convert.ToDateTime(tmp);
             inDate = datePickerIn.Value;
             outDate = datePickerOut.Value.Date;
+
+            FillTableStatistic();
         }
 
+        /// <summary>
+        /// get data from Booking and BookingDetail table and fill data to table
+        /// </summary>
         private void FillTableStatistic()
         {
-            DataTable dt = control.Statistic(hotelCode, roomType, inDate, outDate);
-            dataGridViewStatistic.DataSource = dt;
-            double total = 0;
-            for (int i = 0; i < dt.Rows.Count; i++)
+            if (dataTable != null)
             {
-                total += double.Parse(dt.Rows[i][5].ToString());
+                dataTable.Dispose();
+            }
+            dataTable = control.Statistic(hotelCode, roomType, inDate, outDate);
+            dataGridViewStatistic.DataSource = dataTable;
+            double total = 0;
+            for (int i = 0; i < dataTable.Rows.Count; i++)
+            {
+                total += double.Parse(dataTable.Rows[i][5].ToString());
             }
             lblTotalRevenue.Text = "Total revenue: " + total + "($)";
+        }
+
+        private void ClearDataTable()
+        {
+            if (dataTable != null)
+            {
+                dataTable.Clear();
+            }
         }
 
         private void datePickerIn_ValueChanged(object sender, EventArgs e)
         {
             inDate = datePickerIn.Value.Date;
+            ClearDataTable();
         }
 
         private void datePickerOut_ValueChanged(object sender, EventArgs e)
         {
             outDate = datePickerOut.Value.Date;
+            ClearDataTable();
         }
 
         private void btnViewRevenue_Click(object sender, EventArgs e)
         {
-            if ((outDate-inDate).Days>0)
+            if ((outDate - inDate).Days > 0)
             {
                 FillTableStatistic();
             }
             else
             {
                 MessageBox.Show("Check-out date must be greater than check-in date");
+                ClearDataTable();
             }
         }
 
@@ -88,11 +109,13 @@ namespace HotelReservationSystem.Administration.View
         private void comboHotel_SelectedIndexChanged(object sender, EventArgs e)
         {
             hotelCode = comboHotel.SelectedValue.ToString();
+            ClearDataTable();
         }
 
         private void comboRoomType_SelectedIndexChanged(object sender, EventArgs e)
         {
             roomType = comboRoomType.SelectedValue.ToString();
+            ClearDataTable();
         }
     }
 }
